2b1746fadd4eb0408d64437f9d6bd3fe37c7775d
Caroline Ryu <93948901+carolineryu1@users.noreply.github.com>
2021-12-26 04:45:08 +0900

    Update models.py

--- models.py
+++ models.py
@@ -4,23 +4,7 @@ Models module for Froggit
 This module contains the model classes for the Frogger game. Anything that you
 interact with on the screen is model: the frog, the cars, the logs, and so on.
 
-Just because something is a model does not mean there has to be a special class for
-it. Unless you need something special for your extra gameplay features, cars and logs
-could just be an instance of GImage that you move across the screen. You only need a new
-class when you add extra features to an object.
-
-That is why this module contains the Frog class.  There is A LOT going on with the
-frog, particularly once you start creating the animation coroutines.
-
-If you are just working on the main assignment, you should not need any other classes
-in this module. However, you might find yourself adding extra classes to add new
-features.  For example, turtles that can submerge underneath the frog would probably
-need a custom model for the same reason that the frog does.
-
-If you are unsure about  whether to make a new class or not, please ask on Piazza. We
-will answer.
-
-Author: Caroline Ryu (jr894)
+Author: Caroline Ryu
 Date: December 19, 2020
 """
 from consts import *
@@ -34,20 +18,7 @@ from game2d import *
 class Frog(GImage):         # You will need to change this by Task 3
     """
     A class representing the frog
-
-    The frog is represented as an image (or sprite if you are doing timed animation).
-    However, unlike the obstacles, we cannot use a simple GImage class for the frog.
-    The frog has to have additional attributes (which you will add).  That is why we
-    make it a subclass of GImage.
-
-    When you reach Task 3, you will discover that Frog needs to be a composite object,
-    tracking both the frog animation and the death animation.  That will like caused
-    major modifications to this class.
     """
-
-    # LIST ALL HIDDEN ATTRIBUTES HERE
-
-    # GETTERS AND SETTERS (ONLY ADD IF YOU NEED THEM)
 
     # INITIALIZER TO SET FROG POSITION
     def __init__(self,x,y):
@@ -65,11 +36,4 @@ class Frog(GImage):         # You will need to change this by Task 3
         Precondition: y is a float value
         """
         super().__init__(x=x, y=y, source = FROG_IMAGE)
-        self.angle = FROG_NORTH
-
-
-    # ADDITIONAL METHODS (DRAWING, COLLISIONS, MOVEMENT, ETC)
-
-
-
-# IF YOU NEED ADDITIONAL LANE CLASSES, THEY GO HERE
+        self.angle = FROG_NORTh
