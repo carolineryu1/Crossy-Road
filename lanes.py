c7a76d439f6de4b8ca63b9220f6da33ec01c5457
Caroline Ryu <93948901+carolineryu1@users.noreply.github.com>
2021-12-26 04:42:37 +0900

    Update lanes.py

--- lanes.py
+++ lanes.py
@@ -4,26 +4,7 @@ Lanes module for Froggit
 This module contains the lane classes for the Frogger game. The lanes are the vertical
 slice that the frog goes through: grass, roads, water, and the exit hedge.
 
-Each lane is like its own level. It has hazards (e.g. cars) that the frog has to make
-it past.  Therefore, it is a lot easier to program frogger by breaking each level into
-a bunch of lane objects (and this is exactly how the level files are organized).
-
-You should think of each lane as a secondary subcontroller.  The level is a subcontroller
-to app, but then that subcontroller is broken up into several other subcontrollers, one
-for each lane.  That means that lanes need to have a traditional subcontroller set-up.
-They need their own initializer, update, and draw methods.
-
-There are potentially a lot of classes here -- one for each type of lane.  But this is
-another place where using subclasses is going to help us A LOT.  Most of your code will
-go into the Lane class.  All of the other classes will inherit from this class, and
-you will only need to add a few additional methods.
-
-If you are working on extra credit, you might want to add additional lanes (a beach lane?
-a snow lane?). Any of those classes should go in this file.  However, if you need additional
-obstacles for an existing lane, those go in models.py instead.  If you are going to write
-extra classes and are now sure where they would go, ask on Piazza and we will answer.
-
-Author: Caroline Ryu (jr894)
+Author: Caroline Ryu 
 Date: December 21, 2020
 """
 from game2d import *
@@ -37,22 +18,11 @@ from models import *
 class Lane(object):         # You are permitted to change the parent class if you wish
     """
     Parent class for an arbitrary lane.
-
-    Lanes include grass, road, water, and the exit hedge.  We could write a class for
-    each one of these four (and we will have classes for THREE of them).  But when you
-    write the classes, you will discover a lot of repeated code.  That is the point of
-    a subclass.  So this class will contain all of the code that lanes have in common,
-    while the other classes will contain specialized code.
-
-    Lanes should use the GTile class and to draw their background.  Each lane should be
-    GRID_SIZE high and the length of the window wide.  You COULD make this class a
-    subclass of GTile if you want.  This will make collisions easier.  However, it can
-    make drawing really confusing because the Lane not only includes the tile but also
-    all of the objects in the lane (cars, logs, etc.)
     """
     pass
-    # LIST ALL HIDDEN ATTRIBUTES HERE
 
+    # Hidden attributes 
+    
     # Attribute _tile: A tiling of one lane using one image
     # Invariant: _tile is a GTile
     #
@@ -83,12 +53,6 @@ class Lane(object):         # You are permitted to change the parent class if yo
     def __init__(self,item,width,l):
         """
         Initializes one lane of the window from the passed-down parameters.
-
-        The initializer appends the position and the source image to the list
-        of objects as well as turns the angle of the objects around if it's less
-        than 0 to make sure it is going on the right direction. It also
-        intializes a tile, a list of safe frogs (when the frog enters an exit),
-        as well as a hitbox dictionary.
 
         Parameter item: a passed down parameter from Level, referring to one lane
         Precondition: item is a dictionary
@@ -161,32 +125,17 @@ class Lane(object):         # You are permitted to change the parent class if yo
 class Grass(Lane):                           # We recommend AGAINST changing this one
     """
     A class representing a 'safe' grass area.
-
-    You will NOT need to actually do anything in this class.  You will only do anything
-    with this class if you are adding additional features like a snake in the grass
-    (which the original Frogger does on higher difficulties).
     """
     pass
-
-    # ONLY ADD CODE IF YOU ARE WORKING ON EXTRA CREDIT EXTENSIONS.
 
 
 class Road(Lane):                           # We recommend AGAINST changing this one
     """
     A class representing a roadway with cars.
-
-    If you implement Lane correctly, you do really need many methods here (not even an
-    initializer) as this class will inherit everything.  However, roads are different
-    than other lanes as they have cars that can kill the frog. Therefore, this class
-    does need a method to tell whether or not the frog is safe.
     """
     def carcollision(self,frog):
         """
         Returns True if a car collides with the frog.
-
-        This method loops over the obstacles in self._objs and use collidse
-        to check if the the collides as long as the frog is still visible
-        on the screen.
 
         Parameter frog: a frog to play the game
         Precondition: frog is a Frog object
@@ -200,13 +149,7 @@ class Road(Lane):                           # We recommend AGAINST changing this
 class Water(Lane):
     """
     A class representing a waterway with logs.
-
-    If you implement Lane correctly, you do really need many methods here (not even an
-    initializer) as this class will inherit everything.  However, water is very different
-    because it is quite hazardous. The frog will die in water unless the (x,y) position
-    of the frog (its center) is contained inside of a log. Therefore, this class needs a
-    method to tell whether or not the frog is safe.
-
+    
     In addition, the logs move the frog. If the frog is currently in this lane, then the
     frog moves at the same rate as all of the logs.
     """
@@ -253,31 +196,10 @@ class Water(Lane):
 class Hedge(Lane):
     """
     A class representing the exit hedge.
-
-    This class is a subclass of lane because it does want to use a lot of the features
-    of that class. But there is a lot more going on with this class, and so it needs
-    several more methods.  First of all, hedges are the win condition. They contain exit
-    objects (which the frog is trying to reach). When a frog reaches the exit, it needs
-    to be replaced by the blue frog image and that exit is now "taken", never to be used
-    again.
-
-    That means this class needs methods to determine whether or not an exit is taken.
-    It also need to take the (x,y) position of the frog and use that to determine which
-    exit (if any) the frog has reached. Finally, it needs a method to determine if there
-    are any available exits at all; once they are taken the game is over.
-
-    These exit methods will require several additional attributes. That means this class
-    (unlike Road and Water) will need an initializer. Remember to user super() to combine
-    it with the initializer for the Lane.
     """
     def __init__(self,item,width,l):
         """
         Initializes one Hedge Lane.
-
-        The initializer appends the position and the source image to the list
-        of objects as well as turns the angle of the objects around if it's less
-        than 0 to make sure it is going on the right direction. It also
-        intializes a tile, a list of safe frogs (when the frog enters an exit),
 
         Parameter item: a passed down parameter from Lane, referring to one lane
         Precondition: item is a dictionary
